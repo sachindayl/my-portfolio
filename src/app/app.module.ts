b495b1e60c1f68a17d479488cc75076e2dd0a6e9
Sachinda Liyanaarachchi <sachinda.yl@gmail.com>
2017-01-09 08:44:10 -0500

    added resume

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
+import { AlertModule } from 'ng2-bootstrap';
 
 import { AppComponent } from './app.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { ResumeComponent } from './resume/resume.component';
+
+const appRoutes: Routes = [
+  { path: '', component: MainpageComponent },
+  { path: 'app-resume', component: ResumeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    ResumeComponent
   ],
   imports: [
+    AlertModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

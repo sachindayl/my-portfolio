a6bc33941a1297f12c003734d1b8430f95a6a655
Sachinda Liyanaarachchi <sachinda.yl@gmail.com>
2017-04-01 14:12:16 -0400

    getting some styling done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
